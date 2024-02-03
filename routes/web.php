fb49f830ddc4224df55cf0d0e00a0464b05dff7a
Gabriel <gabrielrezende23@hotmail.com>
2024-02-03 17:12:33 -0400

    feat : alteracoes no dia 03/02/2024

--- routes/web.php
+++ routes/web.php
@@ -25,8 +25,14 @@ Route::get('/login',function (){ return 'Login'; })->name('site.login');
 
 Route::prefix('/app')->group(function (){
     Route::get('/clientes',function (){ return 'Clientes'; })->name('app.clientes');
-    Route::get('/fornecedores',function (){ return 'Fornecedores'; })->name('site.fornecedores');
-    Route::get('/produtos',function (){ return 'Produtos'; })->name('site.produtos');
+    Route::get('/fornecedores', 'FornecedorController@index')->name('app.fornecedores');
+    Route::get('/produtos',function (){ return 'Produtos'; })->name('app.produtos');
 });
 
+Route::get('/teste/{p1}/{p2}', 'TesteController@teste')->name('teste');
+
+
+Route::fallback(function () {
+    echo 'A rota acessada nao existe. <a href = "'.route('site.index').'"> clique aqui </a> para ir para pagina inicial';
+});
 
