090dfa40d6580d2ee59e8610bdfe330799cee0df
Gabriel <gabrielrezende23@hotmail.com>
2024-02-18 18:53:20 -0400

    feat : alteracoes no dia 18/02/2024

--- resources/views/app/fornecedor/index.blade.php
+++ resources/views/app/fornecedor/index.blade.php
@@ -1,19 +1,29 @@
 <h3>Fornecedor</h3>
 
 @php
-//
-//    if () {
-//
-//    } elseif () {
-//
-//    } else
+    /*
+    if(empty($variavel) {} // retornar true se a variavel estiver vazia
+    -''
+    -0
+    -0.0
+    -'0'
+    -null
+    -false
+    -array()
+    -$var
+    */
 
  @endphp
 
-@if(count($fornecedores) > 0 && count($fornecedores) < 10)
-    <h3>Existem alguns fornecedores cadastrados</h3>
-@elseif(count($fornecedores)> 10)
-    <h3>Existem vários fornecedores cadastrados</h3>
-@else
-    <h3>Ainda não existem fornecedores cadastrados</h3>
-@endif
+@isset($fornecedores)
+    Fornecedores: {{ $fornecedores[0] ['nome'] }}
+    <br>
+    Status: {{ $fornecedores[0] ['status'] }}
+    <br>
+    @isset(  $fornecedores[0] ['cnpj'] )
+        CNPJ: {{ $fornecedores[0] ['cnpj'] }}
+        @empty($fornecedores[0] ['cnpj'])
+            -Vazio
+        @endempty
+    @endisset
+@endisset
