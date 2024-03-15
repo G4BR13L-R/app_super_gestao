6c8d2617963656e7792d1bf36e0e5c7c3f83f126
Gabriel <gabrielrezende23@hotmail.com>
2024-03-14 21:25:18 -0400

    feat : alteracoes no dia 14/03/2024

--- app/Http/Controllers/FornecedorController.php
+++ app/Http/Controllers/FornecedorController.php
@@ -12,13 +12,15 @@ class FornecedorController extends Controller
             0 => [
                 'nome' => 'Fornecedor 1',
                 'status' => 'N',
-                'cnpj' => '00',
+                'cnpj' => '0',
             ],
             1 => [
                 'nome' => 'Fornecedor 2',
                 'status' => 'S',
             ],
         ];
+
+
         return view('app.fornecedor.index', compact('fornecedores'));
     }
 }
